89340b2888a0902d90df14458afcadd24faacca3
minseo-hong <mshong1007@gmail.com>
2024-08-18 12:14:02 +0900

    [Fix] 빌드 오류 수정

--- src/components/DateItem.tsx
+++ src/components/DateItem.tsx
@@ -3,15 +3,27 @@ import clsx from 'clsx';
 interface DateItemProps {
   children: React.ReactNode;
   active?: boolean;
+  className?: string;
+  onClick?: () => void;
 }
 
-export default function DateItem({ children, active }: DateItemProps) {
+export default function DateItem({
+  children,
+  active,
+  className,
+  onClick,
+}: DateItemProps) {
   return (
     <button
-      className={clsx('h-10 w-10 rounded-lg', {
-        'bg-primary-40 text-gray-00': active,
-        'bg-gray-05 text-gray-30': !active,
-      })}
+      className={clsx(
+        'h-10 w-10 rounded-lg',
+        {
+          'bg-primary-40 text-gray-00': active,
+          'bg-gray-05 text-gray-30': !active,
+        },
+        className,
+      )}
+      onClick={onClick}
     >
       {children}
     </button>
