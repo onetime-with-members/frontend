177264d2333021eb6d1f38df96af8ec7d287ca56
minseo-hong <mshong1007@gmail.com>
2024-09-16 17:14:38 +0900

    [Design] 카카오톡 공유하기 텍스트 수정

--- src/components/ShareKakaoButton.tsx
+++ src/components/ShareKakaoButton.tsx
@@ -30,8 +30,7 @@ export default function ShareKakaoButton({ event }: ShareKakaoButtonProps) {
     Kakao.Share.sendDefault({
       objectType: 'feed',
       content: {
-        title: `${event.title} - OneTime`,
-        description: `스케줄 등록 요청이 도착했습니다. 내 스케줄을 등록하고 추천된 시간을 확인해보세요!`,
+        title: `'${event.title}'으로부터 스케줄 등록 요청이 도착했습니다. 내 스케줄을 등록하고 추천된 시간을 확인해보세요.`,
         imageUrl: `${import.meta.env.VITE_SITE_DOMAIN}/images/kakao/kakao-share-thumbnail.png`,
         link: {
           webUrl: url,
