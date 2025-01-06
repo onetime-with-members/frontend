0fd8bda2a3f005ff49f85d1709a488d8ea302db9
minseo-hong <mshong1007@gmail.com>
2025-01-07 07:13:05 +0900

    [Refactor] 토큰 재발급 로직 리팩토링

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,12 @@
 import _axios, { AxiosError } from 'axios';
 
+const accessToken = localStorage.getItem('access-token');
+
 const axios = _axios.create({
   baseURL: import.meta.env.VITE_SERVER_API_URL,
   headers: {
     'Content-Type': 'application/json',
+    Authorization: accessToken ? `Bearer ${accessToken}` : '',
   },
 });
 
@@ -14,42 +17,31 @@ const reissuer = _axios.create({
   },
 });
 
-function removeTokens() {
-  localStorage.removeItem('access-token');
-  localStorage.removeItem('refresh-token');
-  location.reload();
-}
-
-axios.interceptors.request.use(
-  (config) => {
-    const accessToken = localStorage.getItem('access-token');
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
-
 axios.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
+    const originalRequest = error.config;
+
     if (error.response?.status === 401) {
       const refreshToken = localStorage.getItem('refresh-token');
-      if (refreshToken) {
-        try {
-          const res = await reissuer.post('/tokens/action-reissue', {
-            refresh_token: refreshToken,
-          });
 
-          localStorage.setItem('access-token', res.data.payload.access_token);
-          localStorage.setItem('refresh-token', res.data.payload.refresh_token);
-          location.reload();
-        } catch (refreshError) {
-          removeTokens();
+      try {
+        const res = await reissuer.post('/tokens/action-reissue', {
+          refresh_token: refreshToken,
+        });
+
+        localStorage.setItem('access-token', res.data.payload.access_token);
+        localStorage.setItem('refresh-token', res.data.payload.refresh_token);
+
+        axios.defaults.headers.Authorization = `Bearer ${res.data.payload.access_token}`;
+
+        if (originalRequest) {
+          return axios(originalRequest);
         }
-      } else {
-        removeTokens();
+      } catch (refreshError) {
+        localStorage.removeItem('access-token');
+        localStorage.removeItem('refresh-token');
+        location.reload();
       }
     }
     return Promise.reject(error);
