3bee2ccd07485ac23f07f796e84d7cd593ef9b7a
minseo-hong <mshong1007@gmail.com>
2025-01-07 11:32:08 +0900

    [Fix] 토큰 재발급 로직 오류 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,12 +1,9 @@
 import _axios, { AxiosError } from 'axios';
 
-const accessToken = localStorage.getItem('access-token');
-
 const axios = _axios.create({
   baseURL: import.meta.env.VITE_SERVER_API_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: accessToken ? `Bearer ${accessToken}` : '',
   },
 });
 
@@ -17,35 +14,35 @@ const reissuer = _axios.create({
   },
 });
 
+axios.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem('access-token');
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
 axios.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
-    const originalRequest = error.config;
-
     if (error.response?.status === 401) {
-      const refreshToken = localStorage.getItem('refresh-token');
-
       try {
         const res = await reissuer.post('/tokens/action-reissue', {
-          refresh_token: refreshToken,
+          refresh_token: localStorage.getItem('refresh-token'),
         });
 
         localStorage.setItem('access-token', res.data.payload.access_token);
         localStorage.setItem('refresh-token', res.data.payload.refresh_token);
-
-        axios.defaults.headers.Authorization = `Bearer ${res.data.payload.access_token}`;
-
-        if (originalRequest) {
-          originalRequest.headers.Authorization = `Bearer ${res.data.payload.access_token}`;
-
-          return axios(originalRequest);
-        }
+        location.reload();
       } catch (refreshError) {
         localStorage.removeItem('access-token');
         localStorage.removeItem('refresh-token');
         location.reload();
       }
     }
+
     return Promise.reject(error);
   },
 );
