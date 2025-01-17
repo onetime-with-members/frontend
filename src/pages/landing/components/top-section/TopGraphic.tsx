011fcd54fe50061dbeb234635bda6f13f687e0a1
minseo-hong <mshong1007@gmail.com>
2025-01-17 21:53:29 +0900

    [Design] 랜딩 페이지 최상단 섹션 캘린더 아이콘 변경

--- src/pages/landing/components/top-section/TopGraphic.tsx
+++ src/pages/landing/components/top-section/TopGraphic.tsx
@@ -1,10 +1,14 @@
-import calendarImage from '../../../../assets/landing/calendar.png';
+import calendarImage from '../../../../assets/landing/calendar.svg';
 
 export default function TopGraphic() {
   return (
     <div className="relative flex h-[300px] flex-col items-center justify-center overflow-x-hidden">
       <div className="h-[200px] w-[230px]">
-        <img src={calendarImage} alt="캘린더" className="h-full w-full" />
+        <img
+          src={calendarImage}
+          alt="캘린더 아이콘"
+          className="h-full w-full"
+        />
       </div>
       <div
         className="absolute bottom-0 left-1/2 -z-10 h-[825px] w-[825px] -translate-x-1/2 rounded-full"
