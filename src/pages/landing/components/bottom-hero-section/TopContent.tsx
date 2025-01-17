57f43e0af3fcccd9cbe494a247214274ac7c9f71
minseo-hong <mshong1007@gmail.com>
2025-01-17 21:51:47 +0900

    [Design] 랜딩 페이지 하단 히어로 섹션 시계 lottie 추가

--- src/pages/landing/components/bottom-hero-section/TopContent.tsx
+++ src/pages/landing/components/bottom-hero-section/TopContent.tsx
@@ -1,10 +1,12 @@
-import calendarImage from '../../../../assets/landing/hero-calendar.svg';
+import Lottie from 'lottie-react';
+
+import clockLottie from '../../../../assets/lottie/landing-clock.json';
 
 export default function TopContent() {
   return (
     <div className="flex flex-col items-center gap-8">
       <div className="h-[152px] w-[152px]">
-        <img src={calendarImage} alt="시계" className="h-full w-full" />
+        <Lottie animationData={clockLottie} />
       </div>
       <p className="text-center text-gray-00 title-md-300">
         원타임에서 더 이상 스트레스 없는{' '}
