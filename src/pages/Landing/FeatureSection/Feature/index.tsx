cbf880a6175cb8350d195e9b66021cee07528b5f
minseo-hong <mshong1007@gmail.com>
2025-01-17 22:27:11 +0900

    [Design] 랜딩 페이지 기능 섹션 디자인 변경

--- src/pages/Landing/FeatureSection/Feature/index.tsx
+++ src/pages/Landing/FeatureSection/Feature/index.tsx
@@ -12,16 +12,16 @@ export default function Feature({
   image,
 }: FeatureProps) {
   return (
-    <div className="mx-auto flex w-full max-w-[20rem] flex-col items-start py-10">
+    <div className="mx-auto flex w-full max-w-[20rem] flex-col items-center py-10">
       <div className="flex flex-col gap-10">
-        <div className="flex flex-col items-start gap-4">
-          <div className="rounded-full bg-gray-80 px-5 py-2 text-gray-00 text-md-300">
+        <div className="flex flex-col items-center gap-4">
+          <div className="rounded-xl bg-primary-00 px-5 py-2 text-primary-40 text-md-300">
             {badgeTitle}
           </div>
-          <h2 className="text-[1.625rem] font-bold leading-[1.4] text-gray-80">
+          <h2 className="text-center text-[1.625rem] font-bold leading-[1.4] text-gray-80">
             {title}
           </h2>
-          <p className="text-gray-40 text-lg-200">{description}</p>
+          <p className="text-center text-gray-40 text-lg-200">{description}</p>
         </div>
         {image}
       </div>
