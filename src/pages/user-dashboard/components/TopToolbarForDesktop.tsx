2701d44ee0409c6db35d5b87284aaf6a90e56d7a
minseo-hong <mshong1007@gmail.com>
2025-01-17 19:45:55 +0900

    [Design] 유저 대시보드 페이지 스크롤 시 툴바 상단에 고정

--- src/pages/user-dashboard/components/TopToolbarForDesktop.tsx
+++ src/pages/user-dashboard/components/TopToolbarForDesktop.tsx
@@ -1,10 +1,14 @@
+import clsx from 'clsx';
 import { Link } from 'react-router-dom';
 
+import useScroll from '../../../hooks/useScroll';
 import { User } from '../../../types/user.type';
 import axios from '../../../utils/axios';
 import { useQuery } from '@tanstack/react-query';
 
 export default function TopToolbarForDesktop() {
+  const { isScrolling } = useScroll();
+
   const isLoggedIn = !!localStorage.getItem('access-token');
 
   const { data: user, isLoading: isUserLoading } = useQuery<User>({
@@ -17,18 +21,29 @@ export default function TopToolbarForDesktop() {
   });
 
   return (
-    <div className="hidden rounded-t-3xl bg-gray-80 px-6 py-4 text-gray-00 md:block">
-      <div className="mx-auto flex w-full max-w-screen-md items-center justify-between gap-2">
-        <h1 className="flex-1 title-lg-300">
-          {!isUserLoading && `안녕하세요, ${user?.nickname}님`}
-        </h1>
-        <Link
-          to="/events/new"
-          className="hidden rounded-xl bg-primary-50 px-4 py-2 text-gray-00 text-md-200 lg:flex"
-        >
-          이벤트 생성하기
-        </Link>
+    <header className="hidden h-[68px] w-full justify-center md:flex">
+      <div
+        className={clsx(
+          'fixed z-30 mx-auto w-full max-w-[calc(768px+2rem)] bg-gray-00',
+          {
+            'shadow-lg': isScrolling,
+          },
+        )}
+      >
+        <div className="rounded-t-3xl bg-gray-80 px-6 py-4 text-gray-00">
+          <div className="mx-auto flex w-full max-w-screen-md items-center justify-between gap-2">
+            <h1 className="flex-1 title-lg-300">
+              {!isUserLoading && `안녕하세요, ${user?.nickname}님`}
+            </h1>
+            <Link
+              to="/events/new"
+              className="hidden rounded-xl bg-primary-50 px-4 py-2 text-gray-00 text-md-200 lg:flex"
+            >
+              이벤트 생성하기
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
