7cd5f657e612bf99fccc4b8688f934740587f0f7
minseo-hong <mshong1007@gmail.com>
2025-01-17 19:29:23 +0900

    [Design] 이벤트 상세 페이지 스크롤 시 툴바 상단에 고정

--- src/pages/event-detail/components/toolbar/TopToolbar.tsx
+++ src/pages/event-detail/components/toolbar/TopToolbar.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import kakaoIcon from '../../../../assets/kakao-icon.svg';
@@ -22,6 +24,8 @@ export default function TopToolbar({
   setIsDeleteAlertOpen,
   handleShareButtonClick,
 }: TopToolbarProps) {
+  const [isScrolling, setIsScrolling] = useState(false);
+
   const params = useParams<{ eventId: string }>();
   const navigate = useNavigate();
 
@@ -37,45 +41,76 @@ export default function TopToolbar({
     setIsDeleteAlertOpen(true);
   }
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolling(true);
+      } else {
+        setIsScrolling(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isScrolling]);
+
   return (
-    <div className="mx-auto w-full max-w-[calc(768px+2rem)] bg-gray-80 px-6 py-4 md:rounded-t-3xl">
-      <header className="flex items-center justify-between md:h-10">
-        <h1 className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-gray-00 text-lg-300 md:title-sm-300">
-          {event?.title || <>&nbsp;</>}
-        </h1>
-        {!isEventPending && event && (
-          <>
-            {event.event_status === 'CREATOR' && (
-              <div className="flex items-center justify-center md:hidden">
-                <ToolbarMenuDropdown
-                  handleDeleteButtonClick={handleDeleteButtonClick}
-                />
-              </div>
-            )}
-            <div className="hidden items-center gap-2 md:flex">
-              <ToolbarButton variant="primary" onClick={handleShareButtonClick}>
-                <img src={sendIcon} alt="보내기 아이콘" />
-              </ToolbarButton>
-              <ToolbarButton variant="yellow" onClick={handleKakaoShare}>
-                <img src={kakaoIcon} alt="카카오톡 아이콘" />
-              </ToolbarButton>
-              {event.event_status === 'CREATOR' && (
-                <>
-                  <ToolbarButton variant="gray" onClick={handleEditButtonClick}>
-                    <PenIcon />
-                  </ToolbarButton>
-                  <ToolbarButton
-                    variant="danger"
-                    onClick={handleDeleteButtonClick}
-                  >
-                    <IconTrashXFilled />
-                  </ToolbarButton>
-                </>
-              )}
-            </div>
-          </>
+    <header className="flex h-[59px] w-full justify-center md:h-[72px]">
+      <div
+        className={clsx(
+          'fixed z-40 mx-auto w-full max-w-[calc(768px+2rem)] bg-gray-00 md:z-30',
+          {
+            'shadow-lg': isScrolling,
+          },
         )}
-      </header>
-    </div>
+      >
+        <div className="bg-gray-80 px-6 py-4 md:rounded-t-3xl">
+          <div className="flex items-center justify-between md:h-10">
+            <h1 className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-gray-00 text-lg-300 md:title-sm-300">
+              {event?.title || <>&nbsp;</>}
+            </h1>
+            {!isEventPending && event && (
+              <>
+                {event.event_status === 'CREATOR' && (
+                  <div className="flex items-center justify-center md:hidden">
+                    <ToolbarMenuDropdown
+                      handleDeleteButtonClick={handleDeleteButtonClick}
+                    />
+                  </div>
+                )}
+                <div className="hidden items-center gap-2 md:flex">
+                  <ToolbarButton
+                    variant="primary"
+                    onClick={handleShareButtonClick}
+                  >
+                    <img src={sendIcon} alt="보내기 아이콘" />
+                  </ToolbarButton>
+                  <ToolbarButton variant="yellow" onClick={handleKakaoShare}>
+                    <img src={kakaoIcon} alt="카카오톡 아이콘" />
+                  </ToolbarButton>
+                  {event.event_status === 'CREATOR' && (
+                    <>
+                      <ToolbarButton
+                        variant="gray"
+                        onClick={handleEditButtonClick}
+                      >
+                        <PenIcon />
+                      </ToolbarButton>
+                      <ToolbarButton
+                        variant="danger"
+                        onClick={handleDeleteButtonClick}
+                      >
+                        <IconTrashXFilled />
+                      </ToolbarButton>
+                    </>
+                  )}
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
