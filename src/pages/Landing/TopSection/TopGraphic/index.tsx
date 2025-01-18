892a7b8d52bd62fb4bbe1912e4afd93b6029cacd
minseo-hong <mshong1007@gmail.com>
2025-01-18 20:53:10 +0900

    [Design] 랜딩 페이지 상단 섹션 모바일일 때 비디오 배경에 맞게 색 변경되도록 수정

--- src/pages/Landing/TopSection/TopGraphic/index.tsx
+++ src/pages/Landing/TopSection/TopGraphic/index.tsx
@@ -1,34 +1,47 @@
-import { useRef } from 'react';
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 
 export default function TopGraphic() {
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isMobile, setIsMobile] = useState(false);
 
-  const handlePlay = () => {
-    if (videoRef.current) {
-      videoRef.current.play();
-    }
-  };
+  useEffect(() => {
+    const checkDevice = () => {
+      const userAgent = navigator.userAgent;
+      if (/android/i.test(userAgent) || /iPhone|iPad|iPod/i.test(userAgent)) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+
+    checkDevice();
+  }, []);
 
   return (
-    <div className="relative flex h-[500px] flex-col items-center justify-center overflow-x-hidden">
-      <button onClick={handlePlay}>button</button>
+    <div className="relative flex h-[450px] flex-col items-center overflow-x-hidden">
       <div
-        className="h-[350px] w-[350px]"
+        className="h-[400px] w-[400px]"
         style={{ clipPath: 'circle(50% at 50% 50%)' }}
       >
-        <video
-          ref={videoRef}
-          src="/videos/landing-phone-video.mp4"
-          autoPlay
-          loop
-          muted
-          playsInline
-          width="100%"
-          height="100%"
-          style={{ objectFit: 'cover' }}
-        />
+        {isMobile && (
+          <video
+            src="/videos/landing-phone-video.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+        )}
       </div>
-      <div className="absolute bottom-0 left-1/2 -z-10 h-[825px] w-[825px] -translate-x-1/2 rounded-full bg-[#CED3F6]" />
+      <div
+        className={clsx(
+          'absolute bottom-0 left-1/2 -z-10 h-[825px] w-[825px] -translate-x-1/2 rounded-full',
+          {
+            'bg-[#C8D0F9]': isMobile,
+            'bg-[#CED3F6]': !isMobile,
+          },
+        )}
+      />
     </div>
   );
 }
