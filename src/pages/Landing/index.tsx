fd0fc2ad93559016fa86634557b20ce0e975e681
minseo-hong <mshong1007@gmail.com>
2025-01-18 15:40:19 +0900

    [Refactor] import alias @ 설정

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,9 @@
-import NavBar from '../../components/NavBar';
 import BottomHeroSection from './BottomHeroSection';
 import CardSection from './CardSection';
 import FeatureSection from './FeatureSection';
 import TopSection from './TopSection';
 import TypoSection from './TypoSection';
+import NavBar from '@/components/NavBar';
 
 export default function NewLanding() {
   return (
