22fde7a051a3e527b1f908cf3241ae49331c701c
minseo-hong <mshong1007@gmail.com>
2025-01-23 02:15:53 +0900

    [Refactor] 404 페이지 폴더 구조 변경 및 @ 경로 추가

---
+++ src/pages/NotFoundPage/MainContent/index.tsx
@@ -0,0 +1,25 @@
+import notFound404 from '@/assets/not-found-404.svg';
+
+export default function MainContent() {
+  return (
+    <div className="mx-auto flex w-full max-w-screen-md flex-1 items-center justify-center">
+      <div className="flex -translate-y-20 flex-col items-center gap-10">
+        <div>
+          <img src={notFound404} alt="404" />
+        </div>
+        <div className="flex flex-col items-center gap-3">
+          <h1 className="text-primary-50 title-sm-300">
+            요청하신 페이지를 찾을 수 없어요
+          </h1>
+          <p className="text-center text-gray-40 text-md-200">
+            찾으시려는 페이지의 주소가 잘못되었거나
+            <br />
+            주소 변경, 삭제로 이용하실 수 없어요.
+            <br />
+            주소가 올바르게 입력되었는지 확인해주세요.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
