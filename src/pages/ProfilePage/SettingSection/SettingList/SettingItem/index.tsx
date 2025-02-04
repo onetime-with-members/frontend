a7932104a141b718066a3d123d48f4e7a01e774d
minseo-hong <mshong1007@gmail.com>
2025-02-04 18:39:48 +0900

    [Feat] 마이페이지 프로필 정보 개인정보처리방침 및 서비스이용약관 항목 추가

--- src/pages/ProfilePage/SettingSection/SettingList/SettingItem/index.tsx
+++ src/pages/ProfilePage/SettingSection/SettingList/SettingItem/index.tsx
@@ -1,18 +1,28 @@
+import { Link } from 'react-router-dom';
+
 import cn from '@/utils/cn';
 
-interface SettingItemProps extends React.HTMLAttributes<HTMLLIElement> {}
+interface SettingItemProps extends React.HTMLAttributes<HTMLLIElement> {
+  href?: string;
+}
 
 export default function SettingItem({
   children,
   className,
+  href,
   ...rest
 }: SettingItemProps) {
+  const content = href ? <Link to={href}>{children}</Link> : children;
+
   return (
     <li
-      className={cn('flex items-center gap-2 px-6 py-4', className)}
+      className={cn(
+        'flex items-center gap-2 px-6 py-4 text-gray-60 text-md-200',
+        className,
+      )}
       {...rest}
     >
-      {children}
+      {content}
     </li>
   );
 }
--- src/pages/ProfilePage/SettingSection/SettingList/index.tsx
+++ src/pages/ProfilePage/SettingSection/SettingList/index.tsx
@@ -1,21 +1,16 @@
-import { Link } from 'react-router-dom';
-
 import SettingItem from './SettingItem';
 
 export default function SettingList() {
   return (
     <ul>
+      <SettingItem href="#">서비스이용약관</SettingItem>
+      <SettingItem href="#">개인정보처리방침</SettingItem>
       <SettingItem>
-        <span className="text-gray-60 text-md-200">버전 정보</span>
-        <span className="text-primary-40 text-md-200">v 1.4.4</span>
+        <span>버전 정보</span>
+        <span className="text-primary-40">v 1.4.4</span>
       </SettingItem>
-      <SettingItem>
-        <Link
-          to="/withdraw"
-          className="cursor-pointer text-gray-30 text-md-200"
-        >
-          서비스 탈퇴하기
-        </Link>
+      <SettingItem href="/withdraw" className="text-gray-30 text-sm-200">
+        서비스 탈퇴하기
       </SettingItem>
     </ul>
   );
