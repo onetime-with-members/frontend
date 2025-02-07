c2cd6350bb343c30638ae4cc8c538f4b7ecab8c0
minseo-hong <mshong1007@gmail.com>
2025-02-08 01:39:36 +0900

    [Fix] 모든 ‘고정 스케줄’ 워딩을 ‘내 스케줄’로 변경

--- src/pages/LandingPage/CardSection/FixedScheduleCard/index.tsx
+++ src/pages/LandingPage/CardSection/FixedScheduleCard/index.tsx
@@ -10,11 +10,10 @@ export default function FixedScheduleCard() {
           반복 작업 없이 간편하게
         </>
       }
-      badgeTitle="고정 스케줄 등록"
+      badgeTitle="내 스케줄 등록"
       description={
         <>
-          내 고정 스케줄을 미리 등록하면{' '}
-          <br className="hidden min-[300px]:block" />
+          내 스케줄을 미리 등록하면 <br className="hidden min-[300px]:block" />
           불가능한 시간은 자동으로 제외돼요.
         </>
       }
@@ -22,7 +21,7 @@ export default function FixedScheduleCard() {
         <div className="mx-auto w-full max-w-[20rem]">
           <img
             src={cardImage}
-            alt="고정 스케줄 UI 이미지"
+            alt="내 스케줄 UI 이미지"
             className="h-full w-full"
           />
         </div>
--- src/pages/UserDashboardPage/MyScheduleSection/Header/index.tsx
+++ src/pages/UserDashboardPage/MyScheduleSection/Header/index.tsx
@@ -5,7 +5,7 @@ import { IconChevronRight } from '@tabler/icons-react';
 export default function Header() {
   return (
     <header className="flex items-center justify-between gap-3">
-      <h2 className="text-gray-90 title-sm-300">고정 스케줄</h2>
+      <h2 className="text-gray-90 title-sm-300">내 스케줄</h2>
       <Link to="/mypage/schedules" className="flex items-center text-gray-50">
         <span>더 보기</span>
         <span>
