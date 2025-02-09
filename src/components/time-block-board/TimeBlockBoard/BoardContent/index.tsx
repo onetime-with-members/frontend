b94686dba96fb086b75b23e131dd557e94414a75
minseo-hong <mshong1007@gmail.com>
2025-02-10 02:03:27 +0900

    [Fix] 스케줄 등록 페이지 한 타임블록 라인에서만 위아래 선택 가능하도록 수정

--- src/components/time-block-board/TimeBlockBoard/BoardContent/index.tsx
+++ src/components/time-block-board/TimeBlockBoard/BoardContent/index.tsx
@@ -1,6 +1,8 @@
 import TimeBlockLine, { TimeBlockLineProps } from './TimeBlockLine';
 import useDragScroll from '@/hooks/useDragScroll';
+import useTimeBlockFill from '@/hooks/useTimeBlockFill';
 import { EventType } from '@/types/event.type';
+import { TimeType } from '@/types/schedule.type';
 
 interface BoardContentProps {
   boardContentRef: React.RefObject<HTMLDivElement>;
@@ -10,8 +12,12 @@ interface BoardContentProps {
   timePointChunks: string[][];
   event: EventType;
   schedules: TimeBlockLineProps['schedules'];
-  changeTimeBlockStatus: TimeBlockLineProps['changeTimeBlockStatus'];
-  handleDialogOpen: TimeBlockLineProps['handleDialogOpen'];
+  changeTimeBlockStatus: (
+    day: TimeType['time_point'],
+    time: TimeType['times'][0],
+    newStatus: boolean,
+  ) => void;
+  handleDialogOpen: TimeBlockLineProps['onDialogOpen'];
   editable: boolean | undefined;
   innerContentProportion: number;
   isPossibleTime: TimeBlockLineProps['isPossibleTime'];
@@ -40,6 +46,36 @@ export default function BoardContent({
     handleDragEnd,
     handleDragLeave,
   } = useDragScroll<HTMLDivElement>({ ref: boardContentRef });
+  const { clickedTimeBlock, handleTimeBlockClick } = useTimeBlockFill({
+    isFilled,
+    fillTimeBlocks: ({ timePoint, times, isFilling }) =>
+      times.forEach((time) =>
+        changeTimeBlockStatus(timePoint, time, isFilling),
+      ),
+  });
+
+  function timesAllMember(timePoint: string) {
+    let result: string[] = [];
+    schedules.forEach((schedule) => {
+      schedule.schedules.forEach((daySchedule) => {
+        if (daySchedule.time_point === timePoint) {
+          result = [...result, ...daySchedule.times];
+        }
+      });
+    });
+    return result;
+  }
+
+  function isFilled(timePoint: string, time: TimeType['times'][0]) {
+    return timesAllMember(timePoint).includes(time);
+  }
+
+  function isClickedFirstFor(timePoint: string, time: TimeType['times'][0]) {
+    return (
+      clickedTimeBlock.startTime === time &&
+      clickedTimeBlock.timePoint === timePoint
+    );
+  }
 
   return (
     <div
@@ -74,8 +110,11 @@ export default function BoardContent({
               startTime={event.start_time}
               endTime={event.end_time}
               schedules={schedules}
-              changeTimeBlockStatus={changeTimeBlockStatus}
-              handleDialogOpen={handleDialogOpen}
+              onTimeBlockClick={handleTimeBlockClick}
+              onDialogOpen={handleDialogOpen}
+              isFilled={isFilled}
+              isClickedFirstFor={isClickedFirstFor}
+              timesAllMember={timesAllMember}
               editable={editable}
               minWidth={
                 index === timePointChunks.length - 1 ? dayLineWidth : undefined
