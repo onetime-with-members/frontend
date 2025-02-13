82ec3b3bd445bc6c1f103829bb6b57dd27b18d63
minseo-hong <mshong1007@gmail.com>
2025-02-13 23:12:23 +0900

    [Fix] Layout 컴포넌트 Helmet 약관 동의 title에서 일반 title로 수정

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -45,7 +45,9 @@ export default function Layout() {
   return (
     <>
       <Helmet>
-        <title>약관 동의 | OneTime</title>
+        <title>
+          OneTime | 원타임으로 쉽고 빠르게 모두가 되는 시간을 찾으세요
+        </title>
       </Helmet>
       <ScrollToTop />
       <div
