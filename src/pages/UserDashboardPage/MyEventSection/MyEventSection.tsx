7f3058fb9605ea26b034f7f52245e16ed8702a89
minseo-hong <mshong1007@gmail.com>
2025-02-13 11:04:46 +0900

    [Fix] 유저 대시보드 페이지 참여한 이벤트 console.log 제거

--- src/pages/UserDashboardPage/MyEventSection/MyEventSection.tsx
+++ src/pages/UserDashboardPage/MyEventSection/MyEventSection.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -10,10 +9,6 @@ import { IconChevronRight } from '@tabler/icons-react';
 
 export default function MyEventSection() {
   const { myEvents, status } = useSelector((state: RootState) => state.event);
-
-  useEffect(() => {
-    console.log('myEvents:', myEvents);
-  }, [myEvents]);
 
   return (
     <section className="flex flex-col gap-3">
