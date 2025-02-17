1fd3de9e1723347a526d200db9ea49970da5a8ec
minseo-hong <mshong1007@gmail.com>
2025-02-17 15:15:38 +0900

    [Feat] 푸터에 약관 추가

--- src/layouts/Layout/Footer/index.tsx
+++ src/layouts/Layout/Footer/index.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 import logoWhite from '@/assets/logo-white.svg';
 import { FooterContext } from '@/contexts/FooterContext';
@@ -18,24 +19,31 @@ export default function Footer() {
   }, [footerRef]);
 
   return (
-    <footer ref={footerRef} className="bg-gray-80 px-4 pb-10 pt-8">
-      <div className="mx-auto flex max-w-screen-sm flex-col gap-2">
-        <div className="flex items-center justify-between">
-          <div>
-            <img src={logoWhite} alt="원타임 로고" />
+    <footer ref={footerRef} className="bg-gray-80 px-4 pb-20 pt-8">
+      <div className="mx-auto flex max-w-screen-sm flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center justify-between">
+            <div>
+              <img src={logoWhite} alt="원타임 로고" />
+            </div>
+            <a
+              href="https://www.instagram.com/one.time.official/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded-full bg-gray-70 p-2"
+            >
+              <IconBrandInstagram size={20} className="text-gray-40" />
+            </a>
           </div>
-          <a
-            href="https://www.instagram.com/one.time.official/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="rounded-full bg-gray-70 p-2"
-          >
-            <IconBrandInstagram size={20} className="text-gray-40" />
-          </a>
+          <p className="text-gray-20 text-sm-100">
+            ©OneTime. ALL RIGHTS RESERVED
+          </p>
         </div>
-        <p className="text-gray-20 text-sm-100">
-          ©OneTime. ALL RIGHTS RESERVED
-        </p>
+        <div className="flex items-center gap-2 text-gray-40">
+          <Link to="/policy/privacy">개인정보처리방침</Link>
+          <span>|</span>
+          <Link to="/policy/service">서비스 이용 약관</Link>
+        </div>
       </div>
     </footer>
   );
