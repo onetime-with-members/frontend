a591ad7382857b2b5a794fae01aba5ea3a4fed72
minseo-hong <mshong1007@gmail.com>
2025-02-21 01:02:41 +0900

    [Feat] 404 페이지 영어 추가

--- src/pages/NotFoundPage/MainContent/MainContent.tsx
+++ src/pages/NotFoundPage/MainContent/MainContent.tsx
@@ -1,25 +1,33 @@
+import { Trans, useTranslation } from 'react-i18next';
+
 import notFound404 from '@/assets/not-found-404.svg';
 
 export default function MainContent() {
+  const { t } = useTranslation();
+
   return (
-    <div className="mx-auto flex w-full max-w-screen-md flex-1 items-center justify-center">
-      <div className="flex -translate-y-20 flex-col items-center gap-10">
-        <div>
-          <img src={notFound404} alt="404" />
-        </div>
-        <div className="flex flex-col items-center gap-3">
-          <h1 className="text-primary-50 title-sm-300">
-            요청하신 페이지를 찾을 수 없어요
-          </h1>
-          <p className="text-center text-gray-40 text-md-200">
-            찾으시려는 페이지의 주소가 잘못되었거나
-            <br />
-            주소 변경, 삭제로 이용하실 수 없어요.
-            <br />
-            주소가 올바르게 입력되었는지 확인해주세요.
-          </p>
+    <main className="flex flex-1 flex-col px-4">
+      <div className="mx-auto flex w-full max-w-screen-md flex-1 items-center justify-center">
+        <div className="flex -translate-y-20 flex-col items-center gap-10">
+          <div>
+            <img src={notFound404} alt="404" />
+          </div>
+          <div className="flex flex-col items-center gap-3">
+            <h1 className="text-center text-primary-50 title-sm-300">
+              {t('404.title')}
+            </h1>
+            <p className="text-center text-gray-40 text-md-200">
+              <Trans i18nKey="404.description">
+                찾으시려는 페이지의 주소가 잘못되었거나{' '}
+                <br className="hidden xs:block"></br>
+                주소 변경, 삭제로 이용하실 수 없어요.{' '}
+                <br className="hidden xs:block"></br>
+                주소가 올바르게 입력되었는지 확인해주세요.
+              </Trans>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,16 @@
 import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 
 import MainContent from './MainContent/MainContent';
 import NavBar from '@/components/NavBar/NavBar';
 
 export default function NotFoundPage() {
+  const { t } = useTranslation();
+
   return (
     <>
       <Helmet>
-        <title>페이지를 찾을 수 없습니다 | OneTime</title>
+        <title>{t('404.notFound')} | OneTime</title>
       </Helmet>
       <div className="flex flex-1 flex-col">
         <NavBar />
