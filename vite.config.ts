d4bc044ecbb15452db03ccad8953eef856d0e26d
minseo-hong <mshong1007@gmail.com>
2025-03-01 05:43:30 +0900

    [Config] Prerender 빌드 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,40 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 
+import prerender from '@prerenderer/rollup-plugin';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      routes: [
+        '/',
+        '/event/new',
+        '/policy/privacy',
+        '/policy/service',
+        '/login',
+      ],
+      renderer: '@prerenderer/renderer-puppeteer',
+      server: {
+        port: 5173,
+        host: 'localhost',
+      },
+      rendererOptions: {
+        maxConcurrentRoutes: 1,
+        renderAfterTime: 500,
+      },
+      postProcess(renderedRoute) {
+        renderedRoute.html = renderedRoute.html
+          .replace(/http:/i, 'https:')
+          .replace(
+            /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
+            'https://onetime-test.vercel.app/',
+          );
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
