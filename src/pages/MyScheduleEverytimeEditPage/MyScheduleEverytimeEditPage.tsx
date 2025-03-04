6aed886d68508d02451110d78b0857baac49db3b
minseo-hong <mshong1007@gmail.com>
2025-03-05 01:17:52 +0900

    [Feat] 이벤트 수정 페이지 에브리타임 시간표 불러오기 API 연결

--- src/pages/MyScheduleEverytimeEditPage/MyScheduleEverytimeEditPage.tsx
+++ src/pages/MyScheduleEverytimeEditPage/MyScheduleEverytimeEditPage.tsx
@@ -1,10 +1,10 @@
+import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import BottomButton from './BottomButton/BottomButton';
 import MainContent from './MainContent/MainContent';
 import TopAppBar from './TopAppBar/TopAppBar';
-import { DUMMY_EVERYTIME_SCHEDULE } from '@/data/everytime-schedule';
 import { useEverytimeScheduleActions } from '@/stores/everytime-schedule';
 import { EverytimeSchedule } from '@/types/schedule.type';
 import { useMutation } from '@tanstack/react-query';
@@ -20,17 +20,12 @@ export default function MyScheduleEverytimeEditPage() {
   const { mutate: submitEverytimeURL, isPending } =
     useMutation<EverytimeSchedule>({
       mutationFn: async () => {
-        // const res = await axios.get(
-        //   `${import.meta.env.VITE_CRAWLING_SERVER_API_URL}/schedule?url=${everytimeURL}`,
-        // );
-        // console.log(res.data.payload.schedules);
-
-        return new Promise((resolve) =>
-          setTimeout(() => resolve(DUMMY_EVERYTIME_SCHEDULE), 3000),
+        const res = await axios.get(
+          `${import.meta.env.VITE_CRAWLING_SERVER_API_URL}/schedule?url=${everytimeURL}`,
         );
+        return res.data.payload.schedules;
       },
       onSuccess: (data) => {
-        console.log(data);
         setEverytimeSchedule(data);
         navigate('/mypage/schedules/edit');
       },
