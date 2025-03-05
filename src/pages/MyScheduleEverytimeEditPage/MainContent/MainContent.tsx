4c5a1b2ccc42f2f7db3ee06693b1cb4b3ffed822
minseo-hong <mshong1007@gmail.com>
2025-03-06 02:52:23 +0900

    [Feat] 에브리타임 시간표 링크 복사 가이드 영어 버전 추가

--- src/pages/MyScheduleEverytimeEditPage/MainContent/MainContent.tsx
+++ src/pages/MyScheduleEverytimeEditPage/MainContent/MainContent.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next';
 
-import everytimeLinkGuideImage from '@/assets/everytime-link-guide.png';
+import everytimeURLGuideEnImage from '@/assets/everytime-url-guide-en.png';
+import everytimeURLGuideKoImage from '@/assets/everytime-url-guide-ko.png';
 import Input from '@/components/Input/Input';
 
 interface MainContentProps {
@@ -12,7 +13,7 @@ export default function MainContent({
   everytimeURL,
   onInputChange,
 }: MainContentProps) {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   return (
     <div className="mx-auto flex max-w-screen-sm flex-col gap-6">
@@ -28,7 +29,11 @@ export default function MainContent({
       </div>
       <div className="overflow-hidden rounded-2xl">
         <img
-          src={everytimeLinkGuideImage}
+          src={
+            i18n.language === 'ko'
+              ? everytimeURLGuideKoImage
+              : everytimeURLGuideEnImage
+          }
           alt="The guide image shows how to get the timetable link on Everytime"
         />
       </div>
