dca0e92dffa2d9a031a05ab904b4eadcbdaec04c
minseo-hong <mshong1007@gmail.com>
2025-03-07 21:29:18 +0900

    [Fix] 내 스케줄 수정 페이지 에브리타임 시간표 등록 시 뒤로가기 알림 활성화되도록 수정

--- src/pages/MyScheduleEditPage/MyScheduleEditPage.tsx
+++ src/pages/MyScheduleEditPage/MyScheduleEditPage.tsx
@@ -107,6 +107,7 @@ export default function MyScheduleEditPage() {
     );
     if (everytimeSchedule.length > 0) {
       setEverytimeSchedule([]);
+      setIsMyScheduleEdited(true);
       toast(t('toast.everytime'));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
