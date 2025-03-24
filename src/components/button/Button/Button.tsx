7002ec4e45bb73926d6634b6d929100fd43757b0
minseo-hong <mshong1007@gmail.com>
2025-03-24 16:24:51 +0900

    [Feat] 탈퇴 페이지 빨간 버튼 인터랙션 색 변경

--- src/components/button/Button/Button.tsx
+++ src/components/button/Button/Button.tsx
@@ -19,7 +19,7 @@ export default function Button({
       className={cn(
         'flex h-14 items-center justify-center rounded-2xl px-4 text-gray-00 duration-150 text-lg-200 disabled:bg-gray-10 disabled:text-gray-30',
         {
-          'bg-danger-50 hover:bg-danger-40 active:bg-danger-40':
+          'bg-danger-50 hover:bg-danger-60 active:bg-danger-60':
             variant === 'danger',
           'bg-gray-80 hover:bg-gray-90 active:bg-gray-90': variant === 'dark',
           'bg-gray-90 hover:bg-[#0c0c0f] active:bg-[#0c0c0f]':
