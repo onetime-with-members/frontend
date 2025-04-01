b3f8e5ef592fc7779aee47c32d90a37ec8882e91
minseo-hong <mshong1007@gmail.com>
2025-04-01 13:24:32 +0900

    [Feat] 띠배너 API 연결

--- src/components/BarBanner/BarBanner.tsx
+++ src/components/BarBanner/BarBanner.tsx
@@ -1,8 +1,14 @@
 'use client';
 
+import { useLocale } from 'next-intl';
+import { useEffect } from 'react';
+
 import { useBarBanner, useBarBannerActions } from '@/stores/bar-banner';
+import { Banner } from '@/types/banner.type';
+import axios from '@/utils/axios';
 import cn from '@/utils/cn';
 import { IconX } from '@tabler/icons-react';
+import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 
 export default function BarBanner({
@@ -15,18 +21,39 @@ export default function BarBanner({
   const isShown = useBarBanner();
   const { hideBarBanner } = useBarBannerActions();
 
+  const { data, isError } = useQuery<Banner>({
+    queryKey: ['banners', 'activated'],
+    queryFn: async () => {
+      const res = await axios.get('/banners/activated');
+      return res.data.payload;
+    },
+    retry: false,
+  });
+
+  const locale = useLocale();
+
   function handleClose() {
     hideBarBanner();
   }
 
+  useEffect(() => {
+    if (isError) {
+      hideBarBanner();
+    }
+  }, [isError, hideBarBanner]);
+
   return (
+    data &&
     isShown && (
       <div className={className}>
         <div
           className={cn(
-            'flex items-center justify-between bg-primary-40 p-4',
+            'flex items-center justify-between p-4',
             innnerClassName,
           )}
+          style={{
+            backgroundColor: data.background_color_code,
+          }}
         >
           <div className="flex items-center gap-2">
             <Image
@@ -35,11 +62,21 @@ export default function BarBanner({
               width={20}
               height={20}
             />
-            <span className="text-gray-00 text-md-300">
-              개강 필수 에브리타임 연동기능 출시!
+            <span
+              className="overflow-hidden text-ellipsis whitespace-nowrap text-md-300"
+              style={{
+                color: data.text_color_code,
+              }}
+            >
+              {locale === 'ko' ? data.content_kor : data.content_eng}
             </span>
           </div>
-          <button className="text-primary-10" onClick={handleClose}>
+          <button
+            onClick={handleClose}
+            style={{
+              color: data.text_color_code,
+            }}
+          >
             <IconX size={20} />
           </button>
         </div>
