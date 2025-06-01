8660b266ec0571ba3e02fd3907634fb7cc894484
minseo-hong <mshong1007@gmail.com>
2025-06-02 06:18:46 +0900

    [Fix] 미들웨어의 토큰 재발급 테스트를 위한 axios 비활성화

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,87 +1,91 @@
-import _axios, { AxiosError, AxiosRequestConfig } from 'axios';
-import { deleteCookie, getCookie, setCookie } from 'cookies-next';
-import dayjs from 'dayjs';
-
-const axios = _axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-const reissuer = _axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-async function removeTokens() {
-  deleteCookie('access-token');
-  deleteCookie('refresh-token');
-  window.location.reload();
-}
-
-axios.interceptors.request.use(
-  async (config) => {
-    const accessToken = getCookie('access-token');
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
-
-axios.interceptors.response.use(
-  (response) => response,
-  async (_error: AxiosError) => {
-    const error = _error as AxiosError & {
-      response: {
-        status: number;
-        data: { code: string };
-      };
-    };
-
-    const originalRequest = error.config as AxiosRequestConfig;
-
-    if (error.response && error.response.status === 401) {
-      const refreshToken = getCookie('refresh-token');
-      if (refreshToken) {
-        try {
-          const { data } = await reissuer.post('/tokens/action-reissue', {
-            refresh_token: refreshToken,
-          });
-
-          setCookie('access-token', data.payload.access_token, {
-            expires: dayjs().add(1, 'year').toDate(),
-          });
-          setCookie('refresh-token', data.payload.refresh_token, {
-            expires: dayjs().add(1, 'year').toDate(),
-          });
-
-          if (originalRequest.headers) {
-            originalRequest.headers.Authorization = `Bearer ${data.payload.access_token}`;
-          }
-
-          return axios(originalRequest);
-        } catch (error) {
-          console.error(error);
-          removeTokens();
-        }
-      } else {
-        removeTokens();
-      }
-    } else if (
-      error.response &&
-      error.response.status === 404 &&
-      error.response.data.code === 'USER-001'
-    ) {
-      removeTokens();
-    }
-
-    return Promise.reject(error);
-  },
-);
+import axios from 'axios';
 
 export default axios;
+
+// import _axios, { AxiosError, AxiosRequestConfig } from 'axios';
+// import { deleteCookie, getCookie, setCookie } from 'cookies-next';
+// import dayjs from 'dayjs';
+
+// const axios = _axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// const reissuer = _axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// async function removeTokens() {
+//   deleteCookie('access-token');
+//   deleteCookie('refresh-token');
+//   window.location.reload();
+// }
+
+// axios.interceptors.request.use(
+//   async (config) => {
+//     const accessToken = getCookie('access-token');
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error),
+// );
+
+// axios.interceptors.response.use(
+//   (response) => response,
+//   async (_error: AxiosError) => {
+//     const error = _error as AxiosError & {
+//       response: {
+//         status: number;
+//         data: { code: string };
+//       };
+//     };
+
+//     const originalRequest = error.config as AxiosRequestConfig;
+
+//     if (error.response && error.response.status === 401) {
+//       const refreshToken = getCookie('refresh-token');
+//       if (refreshToken) {
+//         try {
+//           const { data } = await reissuer.post('/tokens/action-reissue', {
+//             refresh_token: refreshToken,
+//           });
+
+//           setCookie('access-token', data.payload.access_token, {
+//             expires: dayjs().add(1, 'year').toDate(),
+//           });
+//           setCookie('refresh-token', data.payload.refresh_token, {
+//             expires: dayjs().add(1, 'year').toDate(),
+//           });
+
+//           if (originalRequest.headers) {
+//             originalRequest.headers.Authorization = `Bearer ${data.payload.access_token}`;
+//           }
+
+//           return axios(originalRequest);
+//         } catch (error) {
+//           console.error(error);
+//           removeTokens();
+//         }
+//       } else {
+//         removeTokens();
+//       }
+//     } else if (
+//       error.response &&
+//       error.response.status === 404 &&
+//       error.response.data.code === 'USER-001'
+//     ) {
+//       removeTokens();
+//     }
+
+//     return Promise.reject(error);
+//   },
+// );
+
+// export default axios;
