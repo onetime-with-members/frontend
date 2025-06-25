2305a5b09f096e443e4aecfa6c14798a46236be9
minseo-hong <mshong1007@gmail.com>
2025-06-25 22:23:13 +0900

    [Refactor] 내 스케줄 페이지 리액트 쿼리 기반으로 수정

---
+++ src/app/(auth)/mypage/(tab)/schedules/my-schedule.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import SleepIcon from '@/components/icon/sleep';
+import MyTimeBlockBoard from '@/components/time-block-board/my-schedule';
+import { defaultMySchedule } from '@/lib/constants';
+import { myScheduleQueryOption, sleepTimeQueryOption } from '@/lib/query-data';
+import { useQuery } from '@tanstack/react-query';
+
+export default function MySchedulePage() {
+  const { data: mySchedule } = useQuery({ ...myScheduleQueryOption });
+  const { data: sleepTime } = useQuery({ ...sleepTimeQueryOption });
+
+  return (
+    <div className="mx-auto w-full max-w-screen-md pb-32">
+      {/* Sleep Time */}
+      <div className="sticky top-[64px] z-10 flex h-[56px] items-center gap-1.5 rounded-t-2xl bg-primary-00 px-5 py-4 md:top-[122px]">
+        <span>
+          <SleepIcon fill="#4C65E5" size={20} />
+        </span>
+        <span className="text-primary-50 text-md-300">
+          {sleepTime.sleep_start_time} - {sleepTime.sleep_end_time}
+        </span>
+      </div>
+
+      {/* Time Block Board */}
+      <MyTimeBlockBoard
+        mode="view"
+        backgroundColor="white"
+        mySchedule={mySchedule || defaultMySchedule}
+        className="bg-gray-05 pb-16 pl-2 pr-3"
+        topDateGroupClassName="sticky z-10 bg-gray-05 top-[120px] md:top-[178px]"
+      />
+    </div>
+  );
+}
