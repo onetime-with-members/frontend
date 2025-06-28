f3a683807f34e6c72ac4bbe4e13457b281f39af3
minseo-hong <mshong1007@gmail.com>
2025-06-28 19:14:33 +0900

    [Refactor] 사이드 탭 클라이언트 컴포넌트로 변경

--- src/app/(auth)/mypage/(tab)/layout.tsx
+++ src/app/(auth)/mypage/(tab)/layout.tsx
@@ -1,37 +1,5 @@
-import { DesktopHeader, MobileHeader } from './header';
-import ScrollContainerWrapper from './scroll';
-import SideTab from './side-tab';
-import NavBar from '@/components/nav-bar';
-import { auth, currentUser } from '@/lib/auth-action';
+import TabLayout from './tab-layout';
 
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const user = (await auth()) ? (await currentUser()).user : null;
-
-  return (
-    <>
-      {/* Mobile */}
-      <div className="block md:hidden">
-        <MobileHeader />
-        <main className="pb-20">{children}</main>
-      </div>
-
-      {/* Desktop */}
-      <div className="hidden min-h-screen flex-col md:flex">
-        <NavBar user={user} shadow={false} />
-        <div className="px-4">
-          <div className="mx-auto flex w-full max-w-screen-md gap-10">
-            <SideTab />
-            <main className="relative flex flex-1 flex-col gap-2 pb-20 pt-8">
-              <DesktopHeader />
-              <ScrollContainerWrapper>{children}</ScrollContainerWrapper>
-            </main>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <TabLayout>{children}</TabLayout>;
 }
