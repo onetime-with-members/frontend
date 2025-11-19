821502390acd210a5a98c54c6383ecb92d9ba29a
minseo-hong <mshong1007@gmail.com>
2025-11-20 00:55:34 +0900

    [Fix] 미들웨어 내 홈페이지 리다이렉트 로직 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,9 @@
 import { routing } from './i18n/routing';
-import { auth } from './lib/auth';
 import createMiddleware from 'next-intl/middleware';
-import { getLocale } from 'next-intl/server';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-
-  const localePath = `/${await getLocale()}`;
-
-  if (pathname === localePath) {
-    const { isLoggedIn } = await auth();
-
-    const targetPath = isLoggedIn ? '/dashboard' : '/landing';
-
-    return NextResponse.redirect(
-      new URL(`${localePath}${targetPath}`, request.url),
-    );
-  }
 
   const handleI18nRouting = createMiddleware(routing);
   const response = handleI18nRouting(request);
