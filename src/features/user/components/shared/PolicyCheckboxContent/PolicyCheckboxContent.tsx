d5fe4741e9a82cfbb722ee20cc7b7331a6a01b29
minseo-hong <mshong1007@gmail.com>
2025-12-11 18:56:03 +0900

    [Refactor] 정책 수정 페이지 MVVM 기반 리팩토링 마무리

--- src/features/user/components/shared/PolicyCheckboxContent/PolicyCheckboxContent.tsx
+++ src/features/user/components/shared/PolicyCheckboxContent/PolicyCheckboxContent.tsx
@@ -1,9 +1,8 @@
 import { useTranslations } from 'next-intl';
 
-import Checkbox from './Checkbox';
+import AllCheckItem from './AllCheckItem';
+import CheckItem from './CheckItem';
 import { PolicySchema } from '@/features/user/types';
-import cn from '@/lib/cn';
-import { IconChevronRight } from '@tabler/icons-react';
 
 export default function PolicyCheckboxContent({
   value,
@@ -52,98 +51,6 @@ export default function PolicyCheckboxContent({
           {t('marketing')}
         </CheckItem>
       </div>
-    </div>
-  );
-}
-
-function CheckItem({
-  children,
-  checkedKey,
-  value,
-  setValue,
-  setPageDetail,
-  hasPageDetail,
-}: {
-  children: React.ReactNode;
-  checkedKey: keyof PolicySchema;
-  value: PolicySchema;
-  setValue: (newValue: PolicySchema) => void;
-  setPageDetail: React.Dispatch<
-    React.SetStateAction<keyof PolicySchema | null>
-  >;
-  hasPageDetail?: boolean;
-}) {
-  function handleCheckboxClick(event: React.MouseEvent) {
-    event.stopPropagation();
-    setValue({
-      ...value,
-      [checkedKey]: !value[checkedKey],
-    });
-  }
-
-  function handlePageDetailOpen() {
-    if (checkedKey === 'servicePolicy' || checkedKey === 'privacyPolicy') {
-      setPageDetail(checkedKey);
-    } else {
-      setPageDetail(null);
-    }
-  }
-
-  return (
-    <div
-      className={cn('flex items-center justify-between', {
-        'cursor-pointer': hasPageDetail,
-      })}
-      onClick={handlePageDetailOpen}
-    >
-      <div className="flex items-center gap-3">
-        <Checkbox
-          checked={value[checkedKey] as boolean}
-          onCheck={handleCheckboxClick}
-        />
-        <span className="text-gray-60 text-md-200">{children}</span>
-      </div>
-      {hasPageDetail && (
-        <span>
-          <IconChevronRight className="text-gray-20" />
-        </span>
-      )}
-    </div>
-  );
-}
-
-function AllCheckItem({
-  value,
-  setValue,
-}: {
-  value: PolicySchema;
-  setValue: (newValue: PolicySchema) => void;
-}) {
-  const t = useTranslations('policyEdit');
-
-  function handleAllCheckboxClick() {
-    const isNewAllChecked =
-      !value.servicePolicy || !value.privacyPolicy || !value.marketingPolicy;
-
-    setValue({
-      ...value,
-      servicePolicy: isNewAllChecked,
-      privacyPolicy: isNewAllChecked,
-      marketingPolicy: isNewAllChecked,
-    });
-  }
-
-  return (
-    <div
-      className="flex cursor-pointer items-center gap-3 rounded-xl bg-gray-05 p-4"
-      onClick={handleAllCheckboxClick}
-    >
-      <Checkbox
-        checked={
-          value.servicePolicy && value.privacyPolicy && value.marketingPolicy
-        }
-      />
-      <span className="text-gray-90 text-md-300">{t('all')}</span>
     </div>
   );
 }
