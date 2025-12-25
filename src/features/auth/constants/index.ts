0a59882abd566b342af26feb984b65ca1b68a557
minseo-hong <mshong1007@gmail.com>
2025-12-25 17:17:05 +0900

    [Fix] 탈퇴한 유저일 경우 자동 로그아웃이 안되는 오류 수정 시도

--- src/features/auth/constants/index.ts
+++ src/features/auth/constants/index.ts
@@ -1,3 +1,4 @@
 export const REDIRECT_URL = 'redirect-url';
 
 export const EVENT_TOKEN_EXPIRED = 'token_expired';
+export const EVENT_WITHDRAW = 'withdraw';
--- src/features/auth/hooks/useSignOutWhenTokenExpired/useSignOutWhenTokenExpired.ts
+++ src/features/auth/hooks/useSignOutWhenTokenExpired/useSignOutWhenTokenExpired.ts
@@ -1,20 +1,26 @@
 import { useEffect } from 'react';
 
-import { EVENT_TOKEN_EXPIRED } from '../../constants';
+import { EVENT_TOKEN_EXPIRED, EVENT_WITHDRAW } from '../../constants';
 import { useAuth } from '@/lib/auth';
 
 export default function useSignOutWhenTokenExpired() {
-  const { tokenExpired } = useAuth();
+  const { tokenExpired, withdraw } = useAuth();
 
   useEffect(() => {
     async function handleLogout() {
       await tokenExpired();
     }
 
+    async function handleWithdraw() {
+      await withdraw();
+    }
+
     window.addEventListener(EVENT_TOKEN_EXPIRED, handleLogout);
+    window.addEventListener(EVENT_WITHDRAW, handleWithdraw);
 
     return () => {
       window.removeEventListener(EVENT_TOKEN_EXPIRED, handleLogout);
+      window.removeEventListener(EVENT_WITHDRAW, handleWithdraw);
     };
   }, []);
 }
