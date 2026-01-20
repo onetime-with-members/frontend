4a71bf5d57246379dd82d9612363dc74e245ec70
minseo-hong <mshong1007@gmail.com>
2026-01-20 22:31:18 +0900

    [Fix] 로그인하는 유저의 언어 설정값과 사이트의 언어가 맞지 않을 때 로그인 시 리다이렉트되지 않는 오류 수정 시도

--- src/features/user/hooks/useChangeLocale/useChangeLocale.ts
+++ src/features/user/hooks/useChangeLocale/useChangeLocale.ts
@@ -2,19 +2,25 @@ import { Locale } from 'next-intl';
 import { useTransition } from 'react';
 
 import { usePathname, useRouter } from '@/i18n/navigation';
-import { useParams } from 'next/navigation';
+import { useParams, useSearchParams } from 'next/navigation';
 
 export default function useChangeLocale() {
   const [, startTransition] = useTransition();
   const pathname = usePathname();
   const params = useParams();
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   function changeLocale(locale: Locale) {
     startTransition(() => {
+      const newPathname = searchParams.toString()
+        ? `${pathname}?${searchParams.toString()}`
+        : pathname;
       router.replace(
         // @ts-expect-error -- TypeScript will validate that only known `params`
-        { pathname, params },
+        // are used in combination with a given `pathname`. Since the two will
+        // always match for the current route, we can skip runtime checks.
+        { pathname: newPathname, params },
         { locale, scroll: false },
       );
     });
