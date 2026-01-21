d4260b65f5d129f1cf2f70be91a653cc55adc715
minseo-hong <mshong1007@gmail.com>
2026-01-21 18:28:54 +0900

    [Fix] 세션 쿠키의 값이 JSON이 아닌 잘못된 형식의 값일 때 세션이 삭제되도록 수정

--- src/features/auth/lib/auth.server.ts
+++ src/features/auth/lib/auth.server.ts
@@ -4,12 +4,16 @@ import { Session } from '@/features/auth/types';
 import { cookies } from 'next/headers';
 
 export async function auth() {
-  const cookieStore = await cookies();
-  const sessionCookie = cookieStore.get('session');
+  try {
+    const cookieStore = await cookies();
+    const sessionCookie = cookieStore.get('session');
 
-  const session: Session | null = sessionCookie
-    ? JSON.parse(sessionCookie.value)
-    : null;
+    const session: Session | null = sessionCookie
+      ? JSON.parse(sessionCookie.value)
+      : null;
 
-  return { isLoggedIn: !!sessionCookie, session };
+    return { isLoggedIn: !!sessionCookie, session, isInvalidSession: false };
+  } catch {
+    return { isLoggedIn: false, session: null, isInvalidSession: true };
+  }
 }
