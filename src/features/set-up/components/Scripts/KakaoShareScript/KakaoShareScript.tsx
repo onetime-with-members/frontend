a5f3422b71a621a9d6ba1aefa764b44fa5c2f5c9
minseo-hong <mshong1007@gmail.com>
2026-02-21 21:49:42 +0900

    [Refactor] 루트 레이아웃 폴더 구조 기반으로 리팩토링

--- src/features/set-up/components/Scripts/KakaoShareScript/KakaoShareScript.tsx
+++ src/features/set-up/components/Scripts/KakaoShareScript/KakaoShareScript.tsx
@@ -1,5 +1,12 @@
 import Script from 'next/script';
 
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Kakao: any;
+  }
+}
+
 export default function KakaoShareScript() {
   function onLoad() {
     window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
