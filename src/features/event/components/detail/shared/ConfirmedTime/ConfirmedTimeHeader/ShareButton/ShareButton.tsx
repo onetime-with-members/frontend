68247498bc6c4872fafba9f8a557a8a8ec80994d
minseo-hong <mshong1007@gmail.com>
2026-02-25 16:29:32 +0900

    [Feat] 이벤트 상세 페이지 확정 시간 액션 버튼에 공유 팝업 추가

--- src/features/event/components/detail/shared/ConfirmedTime/ConfirmedTimeHeader/ShareButton/ShareButton.tsx
+++ src/features/event/components/detail/shared/ConfirmedTime/ConfirmedTimeHeader/ShareButton/ShareButton.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+
+import SharePopUp from '../../../SharePopUp';
 import ActionButton from '../ActionButton';
 import { ShareIcon } from '@/components/icon';
 
 export default function ShareButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleOpen() {
+    setIsOpen(true);
+  }
+
   return (
-    <ActionButton>
-      <ShareIcon />
-    </ActionButton>
+    <>
+      <ActionButton onClick={handleOpen}>
+        <ShareIcon />
+      </ActionButton>
+      {isOpen && <SharePopUp setIsOpen={setIsOpen} />}
+    </>
   );
 }
