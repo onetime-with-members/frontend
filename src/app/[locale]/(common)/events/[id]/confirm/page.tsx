fa2f84ff0187e979aa485ad2c6863e3bbb12769c
minseo-hong <mshong1007@gmail.com>
2026-02-26 17:37:09 +0900

    [Feat] 이벤트 확정 페이지 메타 데이터 추가

--- src/app/[locale]/(common)/events/[id]/confirm/page.tsx
+++ src/app/[locale]/(common)/events/[id]/confirm/page.tsx
@@ -1,3 +1,6 @@
+import { Locale } from 'next-intl';
+
+import { fetchEvent } from '@/features/event/api/event.api';
 import {
   eventQueryOptions,
   recommendedTimesQueryOptions,
@@ -9,6 +12,41 @@ import {
   QueryClient,
   dehydrate,
 } from '@tanstack/react-query';
+import { getTranslations } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next/types';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string; locale: Locale }>;
+}): Promise<Metadata> {
+  const { id: eventId, locale } = await params;
+
+  const event = await fetchEvent(eventId);
+
+  if (!event) {
+    const t = await getTranslations({
+      locale,
+      namespace: 'setUp.pages.NotFoundPage',
+    });
+
+    return {
+      title: t('notFound'),
+    };
+  }
+
+  const t = await getTranslations({
+    locale,
+    namespace: 'event.pages.EventConfirmPage',
+  });
+
+  return {
+    title: t('metaTitle', {
+      name: event?.title || '',
+    }),
+  };
+}
 
 export default async function Page({
   params,
@@ -18,6 +56,12 @@ export default async function Page({
   const { id: eventId } = await params;
 
   const queryClient = new QueryClient();
+
+  const event = await queryClient.fetchQuery(eventQueryOptions(eventId));
+
+  if (!event) {
+    notFound();
+  }
 
   await queryClient.prefetchQuery(eventQueryOptions(eventId));
   await queryClient.prefetchQuery(recommendedTimesQueryOptions(eventId));
