08959e3473cff03c8cf5626e453a538efb4040f6
minseo-hong <mshong1007@gmail.com>
2026-02-27 22:57:35 +0900

    [Feat] 이벤트 확정 페이지 처음 접속 시 타임피커가 미리 선택되어 있도록 수정

--- src/features/event/components/confirm/PickersSection/PickerPanel/PickerPanel.tsx
+++ src/features/event/components/confirm/PickersSection/PickerPanel/PickerPanel.tsx
@@ -1,12 +1,10 @@
 import { useTranslations } from 'next-intl';
-import { useState } from 'react';
-import { createPortal } from 'react-dom';
+import { useEffect, useState } from 'react';
 
 import CalendarSelect from '@/components/CalendarSelect';
 import TimeDropdown from '@/components/TimeDropdown';
 import WeekdaySelect from '@/features/event/components/form/EventForm/FormContent/InputContent/DateControl/WeekdaySelect';
 import { SelectedDateTime } from '@/features/event/types';
-import useIsMobile from '@/hooks/useIsMobile';
 import cn from '@/lib/cn';
 
 export default function PickerPanel({
@@ -30,8 +28,6 @@ export default function PickerPanel({
 
   const t = useTranslations('event.pages.EventConfirmPage');
 
-  const isMobile = useIsMobile();
-
   const isDisabled = !selectedDateTime.date || !selectedDateTime.time;
 
   const setRanges = (ranges: string[]) => {
@@ -43,10 +39,14 @@ export default function PickerPanel({
     });
   };
 
-  const content = (
+  useEffect(() => {
+    setCurrentRanges(selectedDateTime.date ? [selectedDateTime.date] : []);
+  }, [selectedDateTime.date]);
+
+  return (
     <div
       className={cn(
-        'absolute -bottom-3 flex w-[394px] translate-y-full flex-col gap-5 rounded-[20px] bg-gray-00 p-5 shadow-[0_6px_40px_0_rgba(49,51,63,0.20)] md:static md:bottom-auto md:w-auto md:w-full md:translate-y-0 md:border md:border-gray-10 md:shadow-none',
+        'absolute -bottom-3 flex w-[394px] translate-y-full flex-col gap-5 rounded-[20px] bg-gray-00 p-5 shadow-[0_6px_40px_0_rgba(49,51,63,0.20)] md:static md:bottom-auto md:w-full md:translate-y-0 md:border md:border-gray-10 md:shadow-none',
         {
           'right-0': type === 'end',
         },
@@ -71,7 +71,7 @@ export default function PickerPanel({
         }
         variant="default"
         displayFormat="12h"
-        className="w-[9rem]"
+        className="w-[9.5rem]"
         placement="top"
         interval="30m"
       />
@@ -94,8 +94,4 @@ export default function PickerPanel({
       </div>
     </div>
   );
-
-  return isMobile
-    ? content
-    : createPortal(content, document.getElementById('desktop-picker-panel')!);
 }
