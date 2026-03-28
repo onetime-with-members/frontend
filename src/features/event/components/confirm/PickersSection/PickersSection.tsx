ee260411b2abe00189a88cec563368c4782152dd
chasyuss <cotn963@gmail.com>
2026-03-29 01:35:45 +0900

    [Fix] 이벤트 확정 페이지 모바일 타임피커 자동 포커스 되지 않도록 다시 돌려놓기

--- src/features/event/components/confirm/PickersSection/PickersSection.tsx
+++ src/features/event/components/confirm/PickersSection/PickersSection.tsx
@@ -28,36 +28,6 @@ export default function PickersSection() {
 
   const { data: event } = useEventQuery(params.id);
 
-  const isStartFilled =
-    Boolean(selectedDateTime.start.date) && Boolean(selectedDateTime.start.time);
-  const isEndFilled =
-    Boolean(selectedDateTime.end.date) && Boolean(selectedDateTime.end.time);
-
-  function isStartActive() {
-    if (activePicker === 'start') return true;
-    if (
-      isMobile &&
-      activePicker === 'none' &&
-      !isStartFilled
-    ) {
-      return true;
-    }
-    return false;
-  }
-
-  function isEndActive() {
-    if (activePicker === 'end') return true;
-    if (
-      isMobile &&
-      activePicker === 'none' &&
-      isStartFilled &&
-      !isEndFilled
-    ) {
-      return true;
-    }
-    return false;
-  }
-
   function dispatchConfirmedTime() {
     if (!dispatch) return;
     if (activePicker === 'start') {
@@ -109,14 +79,14 @@ export default function PickersSection() {
             <ConfirmedTimePickerButton
               type="start"
               datePickerType={event.category === 'DATE' ? 'date' : 'day'}
-              active={isStartActive()}
+              active={activePicker === 'start'}
               onClick={() => handlePickerTrigger('start')}
               selectedDateTime={selectedDateTime['start']}
             />
             <ConfirmedTimePickerButton
               type="end"
               datePickerType={event.category === 'DATE' ? 'date' : 'day'}
-              active={isEndActive()}
+              active={activePicker === 'end'}
               onClick={() => handlePickerTrigger('end')}
               selectedDateTime={selectedDateTime['end']}
             />
