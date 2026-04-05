ae51e38478e13eff36cd99c76cdb11a0b302a3fc
minseo-hong <mshong1007@gmail.com>
2026-04-05 18:47:48 +0900

    [Fix] 톡캘린더 일정 생성 성공 팝업 이미지 오류 수정 및 개행 추가

--- src/features/event/components/detail/shared/ConfirmedTime/ConfirmedTime.test.tsx
+++ src/features/event/components/detail/shared/ConfirmedTime/ConfirmedTime.test.tsx
@@ -1,6 +1,6 @@
 import { HttpResponse, http } from 'msw';
 import { NextIntlClientProvider } from 'next-intl';
-import { Mock, expect, it, vi } from 'vitest';
+import { Mock, describe, expect, it, vi } from 'vitest';
 
 import ConfirmedTime from './ConfirmedTime';
 import { createConfirmedEventResponse } from './ConfirmedTime.mock';
@@ -21,96 +21,102 @@ const eventId = faker.string.uuid();
 (useParams as Mock).mockReturnValue({ id: eventId });
 (useProgressRouter as Mock).mockReturnValue({ push: vi.fn() });
 
-it('날짜 이벤트이고 시작 날짜와 종료 날짜가 같을 때, 시작 날짜와 시작 시간 및 종료 시간이 있는 형식으로 확정 시간이 표시된다.', async () => {
-  server.use(
-    http.get(`${SERVER_API_URL}/events/:id`, () => {
-      const response = createConfirmedEventResponse({
-        type: 'DATE',
-        oneday: true,
-      });
-      return HttpResponse.json(response);
-    }),
-  );
-  render(
-    <NextIntlClientProvider locale="ko" messages={messages}>
-      <QueryClientProvider client={new QueryClient()}>
-        <ConfirmedTime />
-      </QueryClientProvider>
-    </NextIntlClientProvider>,
-  );
-  const timeSummaryButton = await screen.findByText(
-    '02/22 (Sun) 07:00 - 23:00',
-  );
+describe('ConfirmedTime', () => {
+  it('날짜 이벤트이고 시작 날짜와 종료 날짜가 같을 때, 시작 날짜와 시작 시간 및 종료 시간이 있는 형식으로 확정 시간이 표시된다.', async () => {
+    server.use(
+      http.get(`${SERVER_API_URL}/events/:id`, () => {
+        return HttpResponse.json(
+          createConfirmedEventResponse({
+            type: 'DATE',
+            oneday: true,
+          }),
+        );
+      }),
+    );
+    render(
+      <NextIntlClientProvider locale="ko" messages={messages}>
+        <QueryClientProvider client={new QueryClient()}>
+          <ConfirmedTime />
+        </QueryClientProvider>
+      </NextIntlClientProvider>,
+    );
+    const timeSummaryButton = await screen.findByText(
+      '02/22 (Sun) 07:00 - 23:00',
+    );
 
-  expect(timeSummaryButton).toBeInTheDocument();
-});
+    expect(timeSummaryButton).toBeInTheDocument();
+  });
 
-it('날짜 이벤트이고 시작 날짜와 종료 날짜가 다를 때, 시작 날짜와 종료 날짜 모두 있는 형식으로 확정 시간이 표시된다.', async () => {
-  server.use(
-    http.get(`${SERVER_API_URL}/events/:id`, () => {
-      const response = createConfirmedEventResponse({
-        type: 'DATE',
-        oneday: false,
-      });
-      return HttpResponse.json(response);
-    }),
-  );
-  render(
-    <NextIntlClientProvider locale="ko" messages={messages}>
-      <QueryClientProvider client={new QueryClient()}>
-        <ConfirmedTime />
-      </QueryClientProvider>
-    </NextIntlClientProvider>,
-  );
-  const timeSummaryButton = await screen.findByText(
-    '02/22(Sun) 07:00 - 02/23(Mon) 23:00',
-  );
+  it('날짜 이벤트이고 시작 날짜와 종료 날짜가 다를 때, 시작 날짜와 종료 날짜 모두 있는 형식으로 확정 시간이 표시된다.', async () => {
+    server.use(
+      http.get(`${SERVER_API_URL}/events/:id`, () => {
+        return HttpResponse.json(
+          createConfirmedEventResponse({
+            type: 'DATE',
+            oneday: false,
+          }),
+        );
+      }),
+    );
+    render(
+      <NextIntlClientProvider locale="ko" messages={messages}>
+        <QueryClientProvider client={new QueryClient()}>
+          <ConfirmedTime />
+        </QueryClientProvider>
+      </NextIntlClientProvider>,
+    );
+    const timeSummaryButton = await screen.findByText(
+      '02/22(Sun) 07:00 - 02/23(Mon) 23:00',
+    );
 
-  expect(timeSummaryButton).toBeInTheDocument();
-});
+    expect(timeSummaryButton).toBeInTheDocument();
+  });
 
-it('요일 이벤트이고 시작 요일과 종료 요일이 같을 때, 시작 요일과 종료 요일이 모두 있는 형식으로 확정 시간이 표시된다.', async () => {
-  server.use(
-    http.get(`${SERVER_API_URL}/events/:id`, () => {
-      const response = createConfirmedEventResponse({
-        type: 'DAY',
-        oneday: true,
-      });
-      return HttpResponse.json(response);
-    }),
-  );
-  render(
-    <NextIntlClientProvider locale="ko" messages={messages}>
-      <QueryClientProvider client={new QueryClient()}>
-        <ConfirmedTime />
-      </QueryClientProvider>
-    </NextIntlClientProvider>,
-  );
-  const timeSummaryButton = await screen.findByText('Monday 07:00 - 23:00');
+  it('요일 이벤트이고 시작 요일과 종료 요일이 같을 때, 시작 요일과 종료 요일이 모두 있는 형식으로 확정 시간이 표시된다.', async () => {
+    server.use(
+      http.get(`${SERVER_API_URL}/events/:id`, () => {
+        return HttpResponse.json(
+          createConfirmedEventResponse({
+            type: 'DAY',
+            oneday: true,
+          }),
+        );
+      }),
+    );
+    render(
+      <NextIntlClientProvider locale="ko" messages={messages}>
+        <QueryClientProvider client={new QueryClient()}>
+          <ConfirmedTime />
+        </QueryClientProvider>
+      </NextIntlClientProvider>,
+    );
+    const timeSummaryButton = await screen.findByText('Monday 07:00 - 23:00');
 
-  expect(timeSummaryButton).toBeInTheDocument();
-});
+    expect(timeSummaryButton).toBeInTheDocument();
+  });
 
-it('요일 이벤트이고 시작 요일과 종료 요일이 다를 때, 시작 요일과 시작 시간과 종료 시간이 있는 형식으로 확정 시간이 표시된다.', async () => {
-  server.use(
-    http.get(`${SERVER_API_URL}/events/:id`, () => {
-      const response = createConfirmedEventResponse({
-        type: 'DAY',
-        oneday: false,
-      });
-      return HttpResponse.json(response);
-    }),
-  );
-  render(
-    <NextIntlClientProvider locale="ko" messages={messages}>
-      <QueryClientProvider client={new QueryClient()}>
-        <ConfirmedTime />
-      </QueryClientProvider>
-    </NextIntlClientProvider>,
-  );
-  const timeSummaryButton = await screen.findByText(
-    'Monday 07:00 - Friday 23:00',
-  );
+  it('요일 이벤트이고 시작 요일과 종료 요일이 다를 때, 시작 요일과 시작 시간과 종료 시간이 있는 형식으로 확정 시간이 표시된다.', async () => {
+    server.use(
+      http.get(`${SERVER_API_URL}/events/:id`, () => {
+        return HttpResponse.json(
+          createConfirmedEventResponse({
+            type: 'DAY',
+            oneday: false,
+          }),
+        );
+      }),
+    );
+    render(
+      <NextIntlClientProvider locale="ko" messages={messages}>
+        <QueryClientProvider client={new QueryClient()}>
+          <ConfirmedTime />
+        </QueryClientProvider>
+      </NextIntlClientProvider>,
+    );
+    const timeSummaryButton = await screen.findByText(
+      'Monday 07:00 - Friday 23:00',
+    );
 
-  expect(timeSummaryButton).toBeInTheDocument();
+    expect(timeSummaryButton).toBeInTheDocument();
+  });
 });
