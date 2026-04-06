891d4bfd0e5f66b7c9e4f5b654ead9e4b1505f8f
minseo-hong <mshong1007@gmail.com>
2026-04-06 20:04:36 +0900

    [Fix] 이벤트 상세 페이지 확정 시간 표시 컴포넌트 카카오톡 버튼 아이콘 사이즈 수정

--- src/features/event/components/detail/shared/ConfirmedTime/ConfirmedTimeHeader/KakaoTalkButton/KakaoTalkButton.tsx
+++ src/features/event/components/detail/shared/ConfirmedTime/ConfirmedTimeHeader/KakaoTalkButton/KakaoTalkButton.tsx
@@ -21,7 +21,7 @@ export default function KakaoTalkButton() {
         className="flex h-9 w-full flex-row items-center justify-center gap-1 rounded-lg bg-[#FAE100] pl-[10px] pr-3 text-gray-90"
         onClick={handleModalOpen}
       >
-        <KakaoTalkIcon fontSize={24} innerfill="" />
+        <KakaoTalkIcon fontSize={16} innerfill="" />
         <span className="text-gray-90 text-sm-200"> 공유하기</span>
       </ActionButton>
       {isModalOpen && <TalkCalendarShareModal onClose={handleModalClose} />}
