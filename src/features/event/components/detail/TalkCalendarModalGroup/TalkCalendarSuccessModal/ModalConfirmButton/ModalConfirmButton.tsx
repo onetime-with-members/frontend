4a8d2b11627dc0a9feb5928d68a7c45647eb7884
minseo-hong <mshong1007@gmail.com>
2026-04-06 20:42:52 +0900

    [Fix] 톡캘린더 일정 생성 성공 팝업 하단 버튼 링크가 요일 이벤트일 때 적용되지 않는 오류 수정

--- src/features/event/components/detail/TalkCalendarModalGroup/TalkCalendarSuccessModal/ModalConfirmButton/ModalConfirmButton.tsx
+++ src/features/event/components/detail/TalkCalendarModalGroup/TalkCalendarSuccessModal/ModalConfirmButton/ModalConfirmButton.tsx
@@ -1,4 +1,9 @@
+import dayjs from 'dayjs';
 import { useTranslations } from 'next-intl';
+
+import { weekdaysShortKo } from '@/constants';
+import { useEventQuery } from '@/features/event/api/event.query';
+import { useParams } from 'next/navigation';
 
 export default function ModalConfirmButton({
   calendarEventId,
@@ -7,11 +12,29 @@ export default function ModalConfirmButton({
   onClick: () => void;
   calendarEventId: string;
 }) {
+  const params = useParams<{ id: string }>();
   const t = useTranslations('event.components.TalkCalendarSuccessModal');
+
+  const { data: event } = useEventQuery(params.id);
+
+  const dayEventISOString = dayjs(event.confirmation?.start_time, 'HH:mm')
+    .day(
+      weekdaysShortKo.findIndex(
+        (weekday) => weekday === event.confirmation?.start_day,
+      ),
+    )
+    .add(1, 'week')
+    .toISOString()
+    .replace(/[-:]/g, '')
+    .replace(/\.\d{3}/, '');
+  const calendarEventIdPath =
+    event.category === 'DAY'
+      ? `${calendarEventId}_${dayEventISOString}`
+      : calendarEventId;
 
   return (
     <a
-      href={`https://calendar.kakao.com/schedule/${calendarEventId}`}
+      href={`https://calendar.kakao.com/schedule/${calendarEventIdPath}`}
       target="_blank"
       role="button"
       className="flex h-14 items-center justify-center rounded-2xl bg-gray-80 text-gray-00 transition-colors duration-150 text-lg-200 hover:bg-gray-90 active:bg-gray-90"
